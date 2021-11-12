8213bb54e1f0404a8916c2ffbc5b0037feace573
Roman <romantut1988@mail.ru>
2021-11-13 01:28:33 +0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <img src="https://www.google.com/search?q=logo&rlz=1C1GCEA_enBY977BY977&sxsrf=AOaemvJohs94nxzmLpealrZ6NUJnDggNLw:1636713067011&tbm=isch&source=iu&ictx=1&fir=XN9WDPibAE_zBM%252CecvEERuaBAKjQM%252C_%253BVUZWq8JQ1A0UsM%252CbfZ9hop94AV7CM%252C_%253Bz20VsyMwKMnQJM%252C-RH1uHEmKKhUJM%252C_%253BnZn0QCizwq2CxM%252C-RH1uHEmKKhUJM%252C_%253B-eDJED2XpumqjM%252CMUnsBNeVrAyn1M%252C_%253BhhRjvFlqI_WeNM%252CYGi_BYkqGVxOGM%252C_%253Bs-Zs4LHx0xi87M%252CgJWjmA8VzZjggM%252C_%253BC6mbfESS1zkCfM%252CEIqhu_9S4fuumM%252C_%253B8cWPH_wbWDvb-M%252C-RH1uHEmKKhUJM%252C_%253Byqfm0vveodR3fM%252CyjlVmAG8kkMNXM%252C_%253BUeCd_fboUxlo7M%252CQ0E1R7tJkTFdfM%252C_%253BdvmmoqPhbEce_M%252C-RH1uHEmKKhUJM%252C_%253BKOA1XcBZwrseXM%252CQ0E1R7tJkTFdfM%252C_&vet=1&usg=AI4_-kTTVzF9Z788xh-o-LcGx2CD1CDCfQ&sa=X&sqi=2&ved=2ahUKEwj_ztSnz5L0AhWoH7kGHUOvCjUQ9QF6BAgFEAE&biw=1536&bih=664&dpr=1.25#imgrc=C6mbfESS1zkCfM"/>
+            </header>
+            <nav>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Mesa
+                </div>
+            </nav>
+            <div>
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
