2cd16eac361c3f00cbd51b657c8497f6ca04551f
Roman <romantut1988@mail.ru>
2021-11-13 02:03:42 +0300

    social network image

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <header></header>
-
-            <nav>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6LDHZ4mvg3wbeNtVZ1DtNBjTOh6klXQ5bVw&usqp=CAU' />
+            </header>
+            <nav className='nav'>
                 <div>
                     Profile
                 </div>
@@ -16,7 +17,7 @@ const App = () => {
                 </div>
             </nav>
             <div>
-                Main content
+                    Main content
             </div>
         </div>
     );
