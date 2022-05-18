200fcba80264069e6db18b7c8d137da441152afd
romantut1988 <romantut1988@mail.ru>
2022-05-19 00:33:38 +0300

    dividing the site into blocks

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            <a>Home</a>
+            <a>New</a>
+            <a>Messages</a>
+        </div>
+    )
+}
