7dd75363f1ce9ca410749ad4efa9addd113b12e4
romantut1988 <romantut1988@mail.ru>
2022-05-19 10:48:25 +0300

    props transfer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import s from "./Header.module.css";
 export const Header = () => {
     return (
         <header className={s.header}>
-            <img src="../img/images.png"/>
+            <img src="https://png.pngtree.com/png-vector/20191201/ourlarge/pngtree-small-objective-icon-circle-png-image_2060818.jpg" alt="logo"/>
         </header>
     )
 }
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import s from "./Post.module.css"
+import s from "./Post.module.css";
 
-export const Post = () => {
+type PropsType = {
+    message: string
+}
+
+export const Post = (props: PropsType) => {
     return (
         <div className={s.item}>
             <img src={"https://avatars.mds.yandex.net/get-zen_doc/4079111/pub_605aacd949f41a2d655e9173_605aafb14807e8528b063e2a/scale_1200"}/>
-            post 1
+            {props.message}
             <div>
                 <span>Like</span>
             </div>
